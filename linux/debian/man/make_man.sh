1750dda83ece791ee5cf10055d41841f967f0526
alexander holupirek <alexander.holupirek @uni-konstanz.de>
2011-02-11 15:38:30 +0000

    Debian rules to build BaseX package (basex_6.5-1_all.deb).
    - The package provides following commands (and manpages) to Debian GNU/Linux
      * basexgui  .......  Standalone local XML database.
                           Graphical User Interface (GUI) mode.
                           Includes data visualizations and XQuery editor.
      * basex  ..........  Standalone local XML database. Command line mode.
      * basexserver  ....  XML database server
      * basexclient  ....  XML database client
    
    - In X11 menues BaseX should appear under Applications/Data Management

---
+++ linux/debian/man/make_man.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [ ! -x /usr/bin/txt2man ]; then
+  echo 'txt2man is used to produce manpages (please consider installing it).';
+  echo '# apt-get install txt2man';
+  exit 1;
+fi
+
+for i in basex basexgui basexclient basexserver; do
+  /usr/bin/txt2man -s1 -t$i -v"The XML Database" ./$i.txt >./$i.1;
+done;
