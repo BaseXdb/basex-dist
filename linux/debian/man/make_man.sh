e9aeeb0fca4a988985a6b4befba50a684c03118a
Alexander Holupirek <alexander@holupirek.de>
2011-07-01 09:00:49 +0200

    Debian build files for v6.7

--- linux/debian/man/make_man.sh
+++ linux/debian/man/make_man.sh
@@ -9,3 +9,6 @@ fi
 for i in basex basexgui basexclient basexserver; do
   /usr/bin/txt2man -s1 -t$i -v"The XML Database" ./$i.txt >./$i.1;
 done;
+
+echo "Fixing '-' in RESTORE command"
+/bin/sed -ie 's/RESTORE \[name /RESTORE \[name\\-/' basex.1
